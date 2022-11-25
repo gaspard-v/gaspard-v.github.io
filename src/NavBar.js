399b8805d42181d6633c5db38d25273e3cc80273
Gaspard <gaspard.vandecasteele@gmail.com>
2022-11-25 17:30:40 +0100

    change colors

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { AppBar } from '@mui/material';
+import { AppBar, Toolbar } from '@mui/material';
+import { Container } from '@mui/system';
 
 function NavBar() {
-	return <AppBar position="static"></AppBar>;
+	return (
+		<AppBar position="static">
+			<Container maxWidth="x1">
+				<Toolbar disableGutters></Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
 
 export default NavBar;
